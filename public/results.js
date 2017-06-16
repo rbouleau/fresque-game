a3c7596dbcb90e7b072d654e9c1d176fcfc659ad
Fabien Zibi <fabien.zibi@gmail.com>
2017-06-16 10:10:56 +0200

    result page

--- public/results.js
+++ public/results.js
@@ -1,1 +1,31 @@
-var database = firebase.database();
+let database = firebase.database();
+let candidates;
+let list = document.getElementById('candidates');
+
+firebase.database().ref('candidates').once('value').then(function(snapshot) {
+  candidates = Object.values(snapshot.val());
+  candidates.sort(function(a, b) {
+    return b.score - a.score;
+  });
+
+  displayResults();
+});
+
+displayResults = () => {
+  for (let candidate of candidates) {
+    let newElement = document.createElement('li');
+    newElement.innerText = `${candidate.firstname} ${candidate.lastname} (${candidate.score})`;
+    list.append(newElement);
+  }
+};
+
+random = () => {
+  let index = Math.floor((Math.random() * candidates.length + 1));
+  let items = list.children;
+  let winner = items[index];
+
+  for (let item of items) {
+    item.classList.remove('winner');
+  }
+  winner.classList.add('winner');
+}
